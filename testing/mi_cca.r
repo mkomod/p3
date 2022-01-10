d3f22c85eb589f9b9b8e9383df481155e0a3e50d
mkomod <mkomodromos01@gmail.com>
2022-01-10 15:34:36 +0000

    include sparse micca

--- testing/mi_cca.r
+++ testing/mi_cca.r
@@ -1,23 +1,26 @@
 # Mutual Information CCA
 library(MASS)
 
+source("./mi_cca_funcs.r")
+
 
 # ----------------------------------------
 # Example 1
 # ----------------------------------------
-p <- 2; q <- 2; n <- 60
+p <- 2; q <- 2; n <- 200
 x <- matrix(nrow=n, ncol=p)
 y <- matrix(nrow=n, ncol=q)
 
 x[, 1] <- rnorm(n)
 x[, 2] <- rnorm(n)
 
-y[, 1] <- x[ , 1]^2
+y[, 1] <- x[ , 1]^2 + x[ ,2]^2
 y[, 2] <- rnorm(n)
 
 cc <- cca(x, y, cca.objective)
 
-for (i in 1:20) {
+for (i in 1:10) {
+    # mi <- cca(x, y, micca.objective_sparse)
     mi <- cca(x, y, micca.objective)
     if (i == 1) maxmi <- mi
     if (mi$val >= maxmi$val) {
@@ -25,7 +28,8 @@ for (i in 1:20) {
     }
 }
 
-maxmi
+plot(maxmi$a %*% t(x), maxmi$b %*% t(y))
+plot(cc$a %*% t(x), cc$b %*% t(y))
 
 
 # ----------------------------------------
@@ -53,4 +57,34 @@ for (i in 1:20) {
 
 cc
 maxmi
+
+
+# ----------------------------------------
+# Example 3
+# ----------------------------------------
+p <- 50; q <- 20; n <- 1000
+p <- 10; q <- 10; n <- 1000
+x <- matrix(nrow=n, ncol=p)
+y <- matrix(nrow=n, ncol=q)
+
+x[, 1] <- rnorm(n)
+x[, 2] <- rnorm(n)
+for (i in 3:p)
+    x[ ,i] <- rnorm(n)
+
+y[, 1] <- x[ , 1]^2
+y[, 2] <- rnorm(n)
+for (i in 3:q)
+    y[ ,i] <- rt(n, 4)
+
+cc <- cca(x, y, cca.objective, iter=1e4)
+
+for (i in 1:20) {
+    mi <- cca(x, y, micca.objective_sparse, iter=4e2)
+    # mi <- cca(x, y, micca.objective)
+    if (i == 1) maxmi <- mi
+    if (mi$val >= maxmi$val) {
+	maxmi <- mi
+    }
+}
 
--- testing/mi_cca_funcs.r
+++ testing/mi_cca_funcs.r
@@ -1,4 +1,4 @@
-
+library(MASS)
 
 # compute the canonical correlation vectors for each x, y
 cca <- function(x, y, objective, iter=1e3) 
@@ -6,7 +6,7 @@ cca <- function(x, y, objective, iter=1e3)
     p <- ncol(x)
     q <- ncol(y)
 
-    pars <- double(p + q) + rnorm(p + q)
+    pars <- double(p + q) + rnorm(p + q, 0, 0.1)
     opt <- optim(pars, objective, x=x, y=y, control=list(fnscale=-1, maxit=iter))
     
     a <- opt$par[1:p] 
@@ -64,3 +64,28 @@ micca.objective <- function(pars, x, y)
 }
 
 
+micca.objective_sparse <- function(pars, x, y, lambda=0.01)
+{
+    p <- ncol(x)
+    q <- ncol(y)
+
+    a <- pars[1:p]
+    b <- pars[(p+1):(q+p)]
+
+    a <- a / sum(sqrt(a^2))
+    b <- b / sum(sqrt(b^2))
+    
+    xa <- x %*% a
+    yb <- y %*% b
+
+    px <- approxfun(density(xa))
+    py <- approxfun(density(yb))
+
+    d.xy <- MASS::kde2d(xa, yb, n=max(50, nrow(x) / 4))
+    pxy <- Vectorize(
+	function(x, y) d.xy$z[length(which(x >= d.xy$x)),  length(which(y >= d.xy$y))]
+    )
+    
+    mean(log(pxy(xa, yb)) - log(px(xa)) - log(py(yb))) - 
+	lambda * sum(abs(a)) - lambda * sum(abs(b))
+}
