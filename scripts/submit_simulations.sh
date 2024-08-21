a0f91dc70e728ed502831bdd2b3c3ddc41006f17
mkomod <mkomodromos01@gmail.com>
2024-08-21 21:24:16 +0100

    up sim setup

--- scripts/submit_simulations.sh
+++ scripts/submit_simulations.sh
@@ -1,7 +1,40 @@
 #!/bin/sh
 
-for S in {1..6}
+for S in $(seq 1 $1)
 do
-    qsub -v "SIM=$S" simulations.pbs
+    for D in {1..4}
+    do
+	qsub -v "SIM=$S, DGP=$D" simulations.pbs
+    done
 done
 
+# for MCMC
+# for S in {1..2}
+# do
+#     qsub -v "SIM=$S" simulations.pbs
+# done
+
+# for S in {1..4}
+# do
+#     qsub -v "SIM=$S" simulations.pbs
+# done
+
+# for S in {1..8}
+# do
+#     qsub -v "SIM=$S" simulations.pbs
+# done
+
+# GENERAL SIMS
+# for S in {1..6}
+# do
+#     qsub -v "SIM=$S" simulations.pbs
+# done
+
+
+# for S in {7..11}
+# do
+#     for D in {1..4}
+#     do
+# 	qsub -v "SIM=$S, DGP=$D" simulations.pbs
+#     done
+# done
